31f5b16a81edb87e093aac65abb11493513ac262
Gary Mendonca <mendonca_gary@ymail.com>
2018-02-07 01:49:10 +0530

    evennode

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -27,7 +27,8 @@ angular.module('myApp.view1', ['ngRoute'])
 
 
 
-    $scope.api = "https://freshprints-api.herokuapp.com"
+    //$scope.api = "https://freshprints-api.herokuapp.com"
+    $scope.api = "http://freshprints.eu-4.evennode.com"
 
     let canvas = new fabric.Canvas('canvas');
     let random = Math.floor((Math.random() * 100) + 1);
